07956cc2a5c5a99d040e75066f63447f6ebcad96
Nafis Rayan <nafisrayan123@gmail.com>
2024-07-31 16:20:58 +0600

    update AppPlayer.ts

--- src/serviceTools/AppPlayer.ts
+++ src/serviceTools/AppPlayer.ts
@@ -1,12 +1,25 @@
+// Importing the TrackPlayer library from react-native-track-player
 import TrackPlayer from 'react-native-track-player';
 
+/**
+ * AppPlayer class encapsulates functionalities related to audio player operations.
+ */
 class AppPlayer {
+    /**
+     * Static property to hold the currently selected track.
+     * This can be null if no track is selected.
+     */
     static selectedTrack: TrackPlayer.Track | null;
 
+    /**
+     * Initializes the audio player with specific options.
+     * This method sets up the player with capabilities for play, pause, stop, and seeking.
+     */
     static initializePlayer = async () => {
         try {
+            // Updating the player options to customize its behavior
             TrackPlayer.updateOptions({
-                stopWithApp: false, // false=> music continues in background even when app is closed
+                stopWithApp: false, // Music continues in the background even when the app is closed
                 // Media controls capabilities
                 capabilities: [
                     TrackPlayer.CAPABILITY_PLAY,
@@ -23,20 +36,27 @@ class AppPlayer {
                 ],
             });
 
+            // Setting up the player
             await TrackPlayer.setupPlayer();
         } catch (e) {
             console.log(e);
-            // to-do handle error
+            // Placeholder for error handling
         }
     };
 
+    /**
+     * Converts seconds into a formatted HH:MM:SS string.
+     * @param {number|string} seconds - The time in seconds to convert.
+     * @returns {string} Formatted time string.
+     */
     static secondsToHHMMSS = (seconds: number | string) => {
-        // credits - https://stackoverflow.com/a/37096512
+        // Ensuring seconds is a number
         seconds = Number(seconds);
         const h = Math.floor(seconds / 3600);
         const m = Math.floor((seconds % 3600) / 60);
         const s = Math.floor((seconds % 3600) % 60);
 
+        // Formatting hours, minutes, and seconds
         const hrs = h > 0 ? (h < 10 ? `0${h}:` : `${h}:`) : '';
         const mins = m > 0 ? (m < 10 ? `0${m}:` : `${m}:`) : '00:';
         const scnds = s > 0 ? (s < 10 ? `0${s}` : s) : '00';
